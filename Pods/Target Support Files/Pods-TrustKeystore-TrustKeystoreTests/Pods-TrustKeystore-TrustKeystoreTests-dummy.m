a021afe81d4351e82f6bde95d6e146c7bb79d0e5
shenfh <sfh@meitu.com>
2018-02-23 17:45:35 +0800

    添加pod依赖文件

---
+++ Pods/Target Support Files/Pods-TrustKeystore-TrustKeystoreTests/Pods-TrustKeystore-TrustKeystoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TrustKeystore_TrustKeystoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_TrustKeystore_TrustKeystoreTests
+@end
---
+++ Pods/Target Support Files/Pods-TrustKeystore/Pods-TrustKeystore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TrustKeystore : NSObject
+@end
+@implementation PodsDummy_Pods_TrustKeystore
+@end
